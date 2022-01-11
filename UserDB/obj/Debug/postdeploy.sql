f60d93a221832750e5e42047efbd927f212f059f
Marlyson Mendes <marlysonmendes@gmail.com>
2022-01-11 02:26:18 -0300

    Estrutura do projeto

---
+++ UserDB/obj/Debug/postdeploy.sql
@@ -0,0 +1,10 @@
+﻿if not exists ( select 1 from dbo.[User])
+begin
+	insert into dbo.[User](FirstName,LastName)
+		values ('Marlyson','Mendes'),
+		('Paulo','Oliveira'),
+		('Caique','Matos'),
+		('André','Santos'),
+		('Henry','Pereira')
+end
+GO
